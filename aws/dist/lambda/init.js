b44fde3f2f1c427d7315b68df380bc2d23ec78b0
Ezequiel Foncubierta <ezequiel.foncubierta@gmail.com>
2017-11-05 19:35:11 +0100

    Delete /dist

--- aws/dist/lambda/init.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = require("../config");
-const store_1 = require("../store");
-const storeRegistry = store_1.initStoreRegistry(config.aws);
-const Hyperdoc = require("hyperdoc-core");
-Hyperdoc.Repository.configure(config, storeRegistry);
-exports.H = Hyperdoc;
--- core/dist/session.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-class HyperdocSession {
-    constructor(userId) {
-        this.userId = userId;
-    }
-}
-exports.HyperdocSession = HyperdocSession;
